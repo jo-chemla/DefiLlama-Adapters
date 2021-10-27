fcd2c05f3de2b0be420ed290a2bbccd42daf17cb
Jonathan Chemla <jchemla@iconem.com>
2021-10-27 13:30:38 +0200

    temp push polymarket

--- projects/polymarket.js
+++ projects/polymarket.js
@@ -41,11 +41,12 @@ query GET_POLYMARKET($skip: Int, $block: Int) {
     }  
 }
 `;
+const {sumTokens} = require('./helper/unwrapLPs')
 
 async function getMarketsLiquidity_graphql(timestamp, block, chainBlocks) {
   let scaledLiquidityParameterSum = BigNumber(0)
   let skip = 0
-  balances = {}
+  const balances = {}
   // Continue querying graph while end not reached. Hard cap: skip = 5000
   while (skip !== -1) { 
     const { conditions } = await request(
@@ -53,15 +54,17 @@ async function getMarketsLiquidity_graphql(timestamp, block, chainBlocks) {
       graphQuery, 
       {skip, block}
     );
-    skip += 1000
     console.log(`${conditions && conditions.length} conditions found for skip: ${skip}`)
+    skip += 1000
     if (conditions && conditions.length > 0) {
       conditions.forEach(condition => {
         condition.fixedProductMarketMakers.forEach(fpmm => {
-          console.log(fpmm)
           scaledLiquidityParameterSum = scaledLiquidityParameterSum.plus(BigNumber(fpmm.scaledLiquidityParameter))
-          const collat = fpmm.collateralToken.id
-          balances[collat] = BigNumber(balances[collat] | 0).plus(ffpm.collateralVolume)
+          const collat = 'polygon:' + fpmm.collateralToken.id
+          // TODO: FIND THE MISSING USDC! NOT CORRECT TO TAKE VOLUME
+          fpmm.collateralVolume = fpmm.collateralVolume < 0? 0 : fpmm.collateralVolume
+          balances[collat] = (balances[collat] ? BigNumber(balances[collat]).plus(BigNumber(fpmm.collateralVolume)) : BigNumber(fpmm.collateralVolume)).toFixed(0)
+          // balances[collat] = BigNumber(balances[collat] | 0).plus(BigNumber(fpmm.collateralVolume | 0)).toFixed(0)
         })
       })
     } 
@@ -72,7 +75,7 @@ async function getMarketsLiquidity_graphql(timestamp, block, chainBlocks) {
   }
   console.log(`${scaledLiquidityParameterSum.div(1e6).toFixed(2)}M scaledLiquidityParameterSum to compare to balances`)
   return balances
-  return scaledLiquidityParameterSum
+  // return scaledLiquidityParameterSum
 }
 
 // After a market resolves, then the winning market participants can withdraw their share based on the redemption rate and their contribution at the closing of the market. All participants do not do it immediately though, so volume of every market should be accounted for in TVL
@@ -97,22 +100,30 @@ async function getMarketsLiquidity_api() {
 async function polygon(timestamp, block, chainBlocks) {
   // Get markets liquidity using API
   //const marketsLiquidity = (await getMarketsLiquidity_api()).times(1e6)
-  const balances = (await getMarketsLiquidity_graphql(timestamp, block, chainBlocks)) // marketsLiquidity.times(1e6)
+  let balances = (await getMarketsLiquidity_graphql(timestamp, block, chainBlocks)) // marketsLiquidity.times(1e6)
 
   // Also account for USDC held in the conditional tokens contract because a lot of positions are held outside of the LPs
   // Corresponds to open positions that should be counted as TVL (since they still represent USDC locked into the conditional tokens contract)
+  /*
   let conditionalTokensUSDC = await sdk.api.erc20.balanceOf({
     target: polygonUsdcContract,
     owner: conditionalTokensContract,
     block: chainBlocks['polygon'],
     chain: 'polygon'
   })
+  */
+  //balances = {}
+  console.log('balances before conditional contract', balances)
+  await sumTokens(balances, [[polygonUsdcContract, conditionalTokensContract]], chainBlocks.polygon, 'polygon', addr=>`polygon:${addr}`)
+  console.log('balances after conditional contract', balances)
+
   //conditionalTokensUSDC = BigNumber(conditionalTokensUSDC.output)
 
   // Total open interest: the conditional tokens are held at 0x4D97DCd97eC945f40cF65F87097ACe5EA0476045 and then each market has it's own contract, the address of which is the id of the FixedProductMarketMaker
   //const tvl = marketsLiquidity.plus(conditionalTokensUSDC).toFixed(0)
   //console.log(`-----\n${marketsLiquidity.div(1e12).toFixed(8)}M of marketsLiquidity \n${conditionalTokensUSDC.div(1e12).toFixed(8)}M of conditionalTokensUSDC \nTVL: ${BigNumber(tvl).div(1e12).toFixed(2)}M\n`)
-  return {['polygon:' + polygonUsdcContract]: conditionalTokensUSDC.output};
+  // return {['polygon:' + polygonUsdcContract]: conditionalTokensUSDC.output}
+  return balances
 }
 
 
@@ -120,6 +131,5 @@ module.exports = {
   polygon: {
     tvl: polygon
   },
-  tvl: polygon,
   methodology: `TVL is the total quantity of USDC held in the conditional tokens contract as well as USDC collateral submitted to every polymarket' markets ever opened - once the markets resolve, participants can withdraw theire share given the redeption rate and their input stake, but they do not all do it.`
 }
